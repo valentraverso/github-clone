99917a456ac9cd6850434e60a5d29e7b8d934adb
Valentino Traverso <vt20032003@live.com.ar>
2023-06-01 05:57:51 +0200

    feat: button main & styles

--- src/UI/pages/Homepage/Homepage.tsx
+++ src/UI/pages/Homepage/Homepage.tsx
@@ -1,11 +1,21 @@
+import { IoIosArrowForward } from "react-icons/io"
+import { Link } from "react-router-dom";
 import './Homepage.css';
 
 function Homepage() {
   return (
     <main className='container-homepage__main'>
-      <p>Hola</p>
+      <section className='container-welcome__section'>
+        <p className='github-welcome__p'>Github Clone</p>
+        <p className='valentino-copyrigth__p'>By Valentino Traverso</p>
+        <div className='container-button__div'>
+          <span className='button-view__span'>
+            <Link>View Github <IoIosArrowForward /></Link>
+          </span>
+        </div>
+      </section>
     </main>
-    )
+  )
 }
 
 export { Homepage }
