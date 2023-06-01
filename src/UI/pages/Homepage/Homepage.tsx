1ee64bda72fb2b903b4bf1aa0badeeb3e0788416
Valentino Traverso <vt20032003@live.com.ar>
2023-06-01 07:17:17 +0200

    add: typewrite & add to text github

--- src/UI/pages/Homepage/Homepage.tsx
+++ src/UI/pages/Homepage/Homepage.tsx
@@ -1,12 +1,21 @@
-import { IoIosArrowForward } from "react-icons/io"
+import { IoIosArrowForward } from "react-icons/io";
 import { Link } from "react-router-dom";
+import Typewriter from "typewriter-effect";
 import './Homepage.css';
 
 function Homepage() {
   return (
     <main className='container-homepage__main'>
       <section className='container-welcome__section'>
-        <p className='github-welcome__p'>Github Clone</p>
+        <p className='github-welcome__p'>
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("Github Clone")
+                .start()
+            }}
+          />
+        </p>
         <p className='valentino-copyrigth__p'>By Valentino Traverso</p>
         <div className='container-button__div'>
           <Link to="/repository">
