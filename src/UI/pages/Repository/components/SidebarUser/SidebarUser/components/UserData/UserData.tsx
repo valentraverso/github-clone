2fc0b6b7d7e95f430e25cf575989e033c873f04b
Valentino Traverso <vt20032003@live.com.ar>
2023-06-02 07:51:58 +0200

    feat: styles sidebar name & followers

--- src/UI/pages/Repository/components/SidebarUser/SidebarUser/components/UserData/UserData.tsx
+++ src/UI/pages/Repository/components/SidebarUser/SidebarUser/components/UserData/UserData.tsx
@@ -9,7 +9,7 @@ interface Props {
 export default function UserData({ name, login, bio }: Props) {
     return (
         <div>
-            <h1>{name}</h1>
+            <h1 className='name-text__h1'>{name}</h1>
             <span className="username-text__span">{login}</span>
             {
                 bio &&
